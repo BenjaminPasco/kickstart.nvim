486de42304bf6bf0b90ce5fb0db28fa3ee5ec389
bpasco <benjamin.pasco@hotmail.fr>
2024-02-18 19:45:43 +0100

    add lualine plugin file

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,16 @@
+-- https://github.com/nvim-lualine/lualine.nvim
+-- nvim bar plugin
+
+return {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'auto',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+}
