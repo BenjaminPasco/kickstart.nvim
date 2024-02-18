efa93e489d62b4d5c76850d214ec6a8c367edfb2
bpasco <benjamin.pasco@hotmail.fr>
2024-02-18 19:43:30 +0100

    add onedark-nvim theme plugin file

--- init.lua
+++ init.lua
@@ -125,20 +125,6 @@ require('lazy').setup({
   { 'folke/which-key.nvim', opts = {} },
 
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    lazy = false,
-    config = function()
-      require('onedark').setup {
-        -- Set a style preset. 'dark' is default.
-        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
-      }
-      require('onedark').load()
-    end,
-  },
-
-  {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
