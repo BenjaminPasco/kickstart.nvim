503336e40d1aa8b690a94dbe5c44fc38cfcf5e13
bpasco <benjamin.pasco@hotmail.fr>
2024-02-18 19:50:12 +0100

    add indent-blankline plugin file

--- init.lua
+++ init.lua
@@ -124,15 +124,6 @@ require('lazy').setup({
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
---
+++ lua/custom/plugins/indent-blankline.lua
@@ -0,0 +1,11 @@
+-- https://github.com/lukas-reineke/indent-blankline.nvim
+-- add indentation lines (even on blanklines does not seems to work)
+
+return {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+}
